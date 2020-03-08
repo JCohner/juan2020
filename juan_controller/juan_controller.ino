c2c1f36e9aee4d022a731c5430185131b4a8c988
Joshua Cho <joshuasrcho@gmail.com>
2020-03-08 16:39:56 -0500

    added working drum spinning code

--- juan_controller/juan_controller.ino
+++ juan_controller/juan_controller.ino
@@ -1,11 +1,13 @@
 #include <ServoESP32.h>
 #include <PS4Controller.h>
 
-static const int MRPin = A2; // Right motor PWM (speed)
+static const int MRPin = A0; // Right motor PWM (speed)
 static const int MLPin = A1; // Left motor PWM (speed)
+static const int DPin = 12;
 
 Servo rightMotor;
 Servo leftMotor;
+Servo drumMotor;
 
 int stop = 1500;
 int l = 1500;
@@ -13,9 +15,8 @@ int r = 1500;
 
 void receivePacket() { 
 
-  int L_mapped,R_mapped;
+  int L_mapped,R_mapped,drum_mapped;
   int lx;
-  
 
   if(PS4.data.analog.stick.lx < 10 && PS4.data.analog.stick.lx > -10){
     lx = 0;
@@ -26,9 +27,11 @@ void receivePacket() {
   
   int L_bm = PS4.data.analog.stick.ly + lx;
   int R_bm = - lx + PS4.data.analog.stick.ly;
+  int drum_bm = PS4.data.analog.button.r2;
   
   L_mapped = map(L_bm, 127, -128, 1000, 2000);
   R_mapped = map(R_bm, 127, -128, 1112, 1888);
+  drum_mapped = map(drum_bm,0,255,1500,2000);                                    
 
 
   /*if(PS4.event.button_down.up){
@@ -49,12 +52,15 @@ void receivePacket() {
  
   leftMotor.writeMicroseconds(L_mapped);
   rightMotor.writeMicroseconds(R_mapped);
+  drumMotor.writeMicroseconds(drum_mapped);
 
-  Serial.print("LEFT ");
-  Serial.println(L_mapped,DEC);
+  
+    
+    
+    
+    Serial.print("r2 button at ");
+    Serial.println(drum_mapped);
 
-  Serial.print("RIGHT ");
-  Serial.println(R_mapped,DEC);
 }
 
 void controllerConnect() {
@@ -66,6 +72,7 @@ void setup() {
   Serial.begin(115200);
   rightMotor.attach(MRPin);
   leftMotor.attach(MLPin);
+  drumMotor.attach(DPin);
   //PS4.attach(receivePacket);
   PS4.attachOnConnect(controllerConnect);
   PS4.begin("01:02:03:04:05:67");
